3a5f4599a00c2aebf57db9e9cc9523b48ceb8d97
Abijeet-Raut <70051498+TroZen-X@users.noreply.github.com>
2020-09-30 22:56:52 +0545

    Update source.c

--- NumberChecker/source.c
+++ NumberChecker/source.c
@@ -58,4 +58,31 @@ void primeComposite(int userChoice)
         printf("\n");
     }
 }
+--------In short
+#include<stdio.h>
+ 
+// int main(){
 
+//     int a = 4;
+//     int prime = 1;
+
+//     for(int i = 2; i< a; i++)
+//     {
+//         if(a%i ==0)
+//         {
+//             prime = 0;
+//             break;
+//         }
+//     }
+//     if(prime == 0)
+//     {
+//         printf("this is not prime");
+//     }
+//     else
+//     {
+//         printf("this is prime");
+//     }
+    
+
+// return 0;
+// }
